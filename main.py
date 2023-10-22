a2384233b0820af5755b57fd7e490f0f61d3e206
zhassulan <zhass.akim123@gmail.com>
2023-10-22 21:11:46 +0600

    test

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ print("Вы заполнили такие данные:")
 full_age = 2023-int(birthday_date)
 
 print(f"Ваще зовут {name} {surname}")
-print(f"Вам {full_age} лет")
+print(f"Вам {full_age} года")
 print(f"Вам ответ к первому вопросу: {status_course}")
 print(f"Ваш ответ к второму вопросу: {course_future}")
 
